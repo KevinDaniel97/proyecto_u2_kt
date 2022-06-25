fcdd1ac9695669fe962efb8f1e9ecf5817e01429
Kevin Toapanta <kevin.toapanta997@gmail.com>
2022-06-24 20:02:55 -0500

    configuraciones Spring Base de datos

---
+++ src/main/java/com/uce/edu/demo/repository/IPersonaJdbcRepository.java
@@ -0,0 +1,10 @@
+package com.uce.edu.demo.repository;
+
+import com.uce.edu.demo.modelo.Persona;
+
+public interface IPersonaJdbcRepository {
+
+	
+	
+	
+}
---
+++ src/main/java/com/uce/edu/demo/repository/IPersonaJdbcRepositoryImpl.java
@@ -0,0 +1,9 @@
+package com.uce.edu.demo.repository;
+
+import com.uce.edu.demo.modelo.Persona;
+
+public class IPersonaJdbcRepositoryImpl implements IPersonaJdbcRepository{
+
+	
+
+}
