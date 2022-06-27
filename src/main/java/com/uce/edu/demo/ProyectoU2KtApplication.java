b83617c96c68c8b2e3d3500e41af2a1551079a8a
Kevin Toapanta <kevin.toapanta997@gmail.com>
2022-06-27 11:47:04 -0500

    configuracion Log4j

--- src/main/java/com/uce/edu/demo/ProyectoU2KtApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2KtApplication.java
@@ -1,20 +1,31 @@
 package com.uce.edu.demo;
 
+
+import org.apache.log4j.LogManager;
+import org.jboss.logging.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProyectoU2KtApplication implements CommandLineRunner{
-
+	
+	
+	private static Logger logJava =Logger.getLogger(ProyectoU2KtApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2KtApplication.class, args);
+		
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		logJava.debug("LOG DEBUG ");
+		logJava.info("LOG INFO ");
+		logJava.warn("LOG WARN ");
+		logJava.error("LOG ERROR ");
+		logJava.fatal("LOG FATAL ");
+		
 	}
 
 }
