218ae6d763fac7cb4c1c0adbbf53397d23adf2d4
Kevin Toapanta <kevin.toapanta997@gmail.com>
2022-06-27 13:03:55 -0500

    configuracion Log4j

--- src/main/java/com/uce/edu/demo/ProyectoU2KtApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2KtApplication.java
@@ -1,8 +1,6 @@
 package com.uce.edu.demo;
 
-
-import org.apache.log4j.LogManager;
-import org.jboss.logging.Logger;
+import org.apache.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
